d13409495ad63ce7f8686d7a2cf335638beb3c59
LilianaMerchan <lilianachikitabeiap14@hotmail.com>
2015-12-01 22:21:44 -0500

    Original.

---
+++ EJERCICIOS/src/VISIBILIDAD/EJERCICIOS_2.java
@@ -0,0 +1,15 @@
+package VISIBILIDAD;
+
+public class EJERCICIOS_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		VISIBILIDAD v1=new VISIBILIDAD();
+		v1.MetodoPublico();
+		//v1.Metodoprivado();
+		v1.MetodoPaquete();
+		
+}
+
+}
---
+++ EJERCICIOS/src/VISIBILIDAD/VISIBILIDAD.java
@@ -0,0 +1,17 @@
+package VISIBILIDAD;
+
+public class VISIBILIDAD {
+
+	public void MetodoPublico() {
+		System.out.println("Metodo Publico");
+		MetodoPrivado();
+	}
+
+	private void MetodoPrivado() {
+		System.out.println("Metodo Privado");
+	
+	}
+void MetodoPaquete(){
+	System.out.println("Metodo Paquete");
+}
+}
